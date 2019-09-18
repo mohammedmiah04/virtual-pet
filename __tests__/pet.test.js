1deb223a111cf87a6ddacf824e76976f19621e00
mohammedmiah04 <mohammedmiah04@hotmail.com>
2019-09-18 11:46:20 +0100

    sep 3 + 4

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,10 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-  it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+  
+
+  it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
   });
-});
+})
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,14 @@
 
 
-function Pet() {}
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+      this.age += 1;
+    };
+  }
+
+  const fido = new Pet('Fido');
+  const rex = new Pet('Rex');
 
 module.exports = Pet;
